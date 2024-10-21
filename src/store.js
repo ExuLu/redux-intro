cf6c17b8f7c18fc7a14cb8176c0a08db91ee9cc6
Alena Sharai <exultantislupus@gmail.com>
2024-10-21 11:32:39 +0400

    feat: create reducer and init state

---
+++ src/store.js
@@ -0,0 +1,31 @@
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: '',
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'account/deposit':
+      return { ...state, balance: state.balance + action.payload };
+    case 'account/withdraw':
+      return { ...state, balance: state.balance - action.payload };
+    case 'account/requestLoan':
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload,
+        balance: state.balance + action.payload,
+      };
+    case 'account/payLoan':
+      if (state.loan <= 0) return state;
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: '',
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+};
