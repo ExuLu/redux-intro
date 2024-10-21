0c95abb83d2306a75e9b78a34ad8146d83ee125c
Alena Sharai <exultantislupus@gmail.com>
2024-10-21 16:10:28 +0400

    feat: add redux in cusomter component

--- src/features/customers/Customer.js
+++ src/features/customers/Customer.js
@@ -1,5 +1,9 @@
-function Customer() {
-  return <h2>👋 Welcome, %NAME%</h2>;
-}
+import { useSelector } from 'react-redux';
+
+const Customer = () => {
+  const name = useSelector((store) => store.customer.fullName);
+
+  return <h2>👋 Welcome{name ? `, ${name}` : ''}!</h2>;
+};
 
 export default Customer;
